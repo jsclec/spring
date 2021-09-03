71cdd7b7c74156ee47333a6f967eca54ff681457
jsclec <jleclear@utexas.edu>
2021-09-03 13:15:22 -0500

    Initial

---
+++ src/test/java/com/learning_spring/Spring5webApp/Spring5webAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning_spring.Spring5webApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
